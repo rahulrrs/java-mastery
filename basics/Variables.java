ade4649d95cfa1f1022cf6e3ad17b2852fce6076
Rahul Reji R <rahulrrreji@gmail.com>
2026-04-26 19:02:32 +0530

    preparation

--- basics/Variables.java
+++ basics/Variables.java
@@ -1,3 +1,5 @@
+
+
 public class Variables{
     public static void main(String[] args){
         //types of datatypes
@@ -8,6 +10,25 @@ public class Variables{
         char e = 'a';
         String f = "rayzen";
         boolean g = true;
-        System.out.println(a+"\n"+b+"\n"+c+"\n"+d+"\n"+e+"\n"+f+"\n"+g);
+        int x =4,y=6;
+        final int myNum = 20;
+        System.out.println(a+"\n"+b+"\n"+c+"\n"+d+"\n"+e+"\n"+f+"\n"+g+"\n"+x+"\n"+y);
+        System.out.println(myNum);
+        char ch = 'r';
+        int conv = (int)ch;
+        System.out.println(conv);
+        String firstname = "Rayzen";
+        String lastname = "Tempest";
+        String fullname = firstname+lastname;
+        System.out.println(fullname);
+        String text = "I am ";
+        int age = 21;
+        String text2 = " Years old";
+        String fulltext = text+age+text2;
+        System.out.println(fulltext);
+        char alphabet = 'q';
+        int number = 2;
+        int combine = alphabet+number;
+        System.out.println(combine);
     }
 }
