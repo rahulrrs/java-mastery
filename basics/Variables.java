1a17ab593ee8fd806c715e81c384a83d15b72b38
Rahul Reji R <rahulrrreji@gmail.com>
2026-04-26 13:45:53 +0530

    Java Basics

---
+++ basics/Variables.java
@@ -0,0 +1,13 @@
+public class Variables{
+    public static void main(String[] args){
+        //types of datatypes
+        int a =12;
+        long b = 2432424234324234L;
+        float c=12.43f;
+        double d = 2123.2313213;
+        char e = 'a';
+        String f = "rayzen";
+        boolean g = true;
+        System.out.println(a+"\n"+b+"\n"+c+"\n"+d+"\n"+e+"\n"+f+"\n"+g);
+    }
+}
