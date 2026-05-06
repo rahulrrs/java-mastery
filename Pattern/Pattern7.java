4b842931f536563d0929bb36ca2bf2c8cf9f5650
Rahul Reji R <rahulrrreji@gmail.com>
2026-05-06 23:52:15 +0530

    06-05-26

---
+++ Pattern/Pattern7.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Pattern7 {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String args[]) {
+        System.out.print("Enter the range: ");
+        int range = sc.nextInt();
+        hollowsquare(range);
+    }
+    public static void hollowsquare(int range) {
+        for (int i = 1; i <=range; i++) {
+            for (int j = 1; j<=range; j++) {
+                if (i==1||j==1||i==range||j==range) {
+                    System.out.print("* ");
+                }
+                else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
