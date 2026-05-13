5cc9afdd071a97de9b71ea422d42cacedc0d4217
Rahul Reji R <rahulrrreji@gmail.com>
2026-05-13 17:35:59 +0530

    13-05-26

---
+++ Pattern/Floyds_triangle.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Floyds_triangle {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.print("Enter the range: ");
+        int range = sc.nextInt();
+        floyds_triangle(range);
+    }
+    public static void floyds_triangle(int range) {
+        int count = 1;
+        for (int i = 1; i<=range; i++) {
+            for (int j = 1; j<=i; j++) {
+                System.out.print(count++ +" ");
+            }
+            System.out.println();
+        }
+    }
+}
